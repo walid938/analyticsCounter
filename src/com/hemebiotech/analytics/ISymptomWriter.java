b403d62b4780eba3ec1bf971f679cc9526ddfdf1
walid938 <w.seghdau@gmail.com>
2020-04-21 22:19:35 +0200

    creation ISymptomWriter.java

---
+++ src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -0,0 +1,21 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+
+/**
+ * 
+ */
+
+
+
+public interface ISymptomWriter {
+	/**
+	 * @throws IOException 
+	 * 
+	 * write a list of symptoms sort alphabetically and count these occurrences
+	 *
+	 */
+	
+	
+	void writeSymptoms () throws IOException;
+}
